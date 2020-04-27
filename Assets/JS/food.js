2191c50aca76991c9edf1be92580fd4eee4da080
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-27 22:06:32 +0800

    Fixed animation problem, using addClass&removeClass and setTimeout.

--- Assets/JS/food.js
+++ Assets/JS/food.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	var btn = $('#findFood');
 	btn.on('click', function() {
-		alert('good');
+		$('#overlay').css('display', 'grid');
+		$('#overlay').addClass('overlay');
+		console.log(overlay);
+		setTimeout(() => {
+			$('#overlay').removeClass('overlay');
+			$('#overlay').css('display', 'none');
+		}, 2500);
 	});
 });
